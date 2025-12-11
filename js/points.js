874a577c8f7677e0c15734405c3aabe26e153b5a
Adam Horelican <horelicanadam@gmail.com>
2025-12-11 13:51:37 +0100

    changes on points.html and points.js

--- js/points.js
+++ js/points.js
@@ -35,28 +35,66 @@ function updateNextReward(points) {
   const nextRewardText = document.querySelector(".summary-goal");
   const nextRewardInPercentages = document.querySelector(".summary-note");
 
+  const cardPizza = document.getElementById("pizza-card");
+  const cardOutdoorNight = document.getElementById("outdoor-card");
+  const cardWellness = document.getElementById("wellness-card");
+
+  const unlockedPizza = document.getElementById("pizza-unlocked");
+  const unlockedOutdoor = document.getElementById("outdoor-unlocked");
+  const unlockedWellness = document.getElementById("wellness-unlocked");
+
 
   let targetPoints = 0;
+  let basePoints = 0;
 
   if (points < REWARD_1) {
     targetPoints = REWARD_1;
+    basePoints = 0;
     nextRewardText.textContent =
       `Next reward: pizza party at ${REWARD_1} points.`;
+
+
+    unlockedPizza.textContent = "Not yet unlocked";
+    unlockedOutdoor.textContent = "Not yet unlocked";
+    unlockedWellness.textContent = "Not yet unlocked";
+
   } else if (points < REWARD_2) {
     targetPoints = REWARD_2;
+    basePoints = REWARD_1;
     nextRewardText.textContent =
       `Next reward: outdoor movie night at ${REWARD_2} points.`;
+    cardPizza.classList.replace('reward-card-next', 'reward-card-done');
+
+    unlockedPizza.textContent = "Already unlocked";
+    unlockedOutdoor.textContent = "Not yet unlocked";
+    unlockedWellness.textContent = "Not yet unlocked";
   } else if (points < REWARD_3) {
     targetPoints = REWARD_3;
+    basePoints = REWARD_2;
     nextRewardText.textContent =
       `Next reward: wellness day at ${REWARD_3} points.`;
+    cardPizza.classList.replace('reward-card-next', 'reward-card-done');
+    cardOutdoorNight.classList.replace('reward-card-next', 'reward-card-done');
+
+    unlockedPizza.textContent = "Already unlocked";
+    unlockedOutdoor.textContent = "Already unlocked";
+    unlockedWellness.textContent = "Not yet unlocked";
   } else {
     nextRewardText.textContent = "All rewards unlocked!";
     nextRewardInPercentages.textContent = "";
+    cardPizza.classList.replace('reward-card-next', 'reward-card-done');
+    cardOutdoorNight.classList.replace('reward-card-next', 'reward-card-done');
+    cardWellness.classList.replace('reward-card-next', 'reward-card-done');
+
+    unlockedPizza.textContent = "Already unlocked";
+    unlockedOutdoor.textContent = "Already unlocked";
+    unlockedWellness.textContent = "Already unlocked";
+
     return;
   }
 
-  const percent = Math.min(Math.round((points / targetPoints) * 100), 100);
+  const percent = Math.min(
+    Math.round(((points - basePoints) / (targetPoints - basePoints)) * 100), 100);
   nextRewardInPercentages.textContent =
     `${percent} percent of the way to the next reward.`;
 }
